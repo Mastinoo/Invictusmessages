ddf63a8c68287e3bb8cb0e9cb3a57dc9ed976e5e
Mastinoo <dylan@fraiquin.eu>
2025-02-01 00:25:49 +0100

    1.07
    not yet

--- commands/setforward.js
+++ commands/setforward.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { loadMappings, saveMappings } = require('../mappings'); // Adjust path to mappings.js
-const { ChannelType } = require('discord.js'); // Import ChannelType for more accurate type checking
+const { loadMappings, saveMappings } = require('../mappings');
+const { ChannelType } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,13 +10,13 @@ module.exports = {
       option.setName('source')
             .setDescription('Source channel')
             .setRequired(true)
-            .addChannelTypes(ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum) // Allow all relevant types
+            .addChannelTypes(ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum)
     )
     .addChannelOption(option => 
       option.setName('target')
             .setDescription('Target channel')
             .setRequired(true)
-            .addChannelTypes(ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum) // Allow all relevant types
+            .addChannelTypes(ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum)
     )
     .addStringOption(option => 
       option.setName('server')
@@ -24,14 +24,16 @@ module.exports = {
     ),
 
   async execute(interaction) {
+    await interaction.deferReply(); // Defer the reply to avoid timeout error
+
     const sourceChannel = interaction.options.getChannel('source');
     const targetChannel = interaction.options.getChannel('target');
-    const serverId = interaction.options.getString('server'); // Optional server ID
+    const serverId = interaction.options.getString('server'); 
 
     // Ensure both channels are valid and of correct types
     if (![ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum].includes(sourceChannel.type) || 
         ![ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread, ChannelType.GuildForum].includes(targetChannel.type)) {
-      return interaction.reply('Both source and target must be text channels, threads, or forum channels!');
+      return interaction.editReply('Both source and target must be text channels, threads, or forum channels!');
     }
 
     // Load existing mappings
@@ -54,8 +56,8 @@ module.exports = {
     // Save the updated mappings
     saveMappings(channelMappings);
 
-    // Acknowledge the setup
-    await interaction.reply(`Messages from ${sourceChannel.name} will now be forwarded to ${targetChannel.name} in the server ${guildId}.`);
+    // Reply once the command has been processed
+    await interaction.editReply(`Messages from ${sourceChannel.name} will now be forwarded to ${targetChannel.name} in the server ${guildId}.`);
     console.log(`Source and target channels for guild ${guildId}:`, channelMappings[guildId]);
   },
 };
